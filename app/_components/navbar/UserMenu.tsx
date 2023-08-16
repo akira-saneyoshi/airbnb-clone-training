b69c7618102ebdc69a6f5365594a4308e22507cb
saneyoshi <aaa0410as@gmail.com>
2023-08-16 14:01:38 +0900

    add:Properties screen (Loading your listings with server component).

--- app/_components/navbar/UserMenu.tsx
+++ app/_components/navbar/UserMenu.tsx
@@ -118,7 +118,7 @@ const UserMenu: React.FC<NavbarProps> = ({
                 />
                 <MenuItem
                   label="My properties"
-                  onClick={() => {}}
+                  onClick={() => router.push('/properties')}
                 />
                 <MenuItem
                   label="Airbnb your home"
